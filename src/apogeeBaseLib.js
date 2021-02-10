96f1ca8e3b6678593bd99bc57ecf2261afd30b37
sutter-dave <sutter@intransix.com>
2021-02-09 18:14:51 -0800

    update to get refactored repos running

--- src/apogeeBaseLib.js
+++ src/apogeeBaseLib.js
@@ -1,3 +1,3 @@
 //This is a single module that exports utility classes and object used internally across the apogee workspace
-export {default as EventManager} from "/apogeeutil/EventManager.js";
-export {default as FieldObject} from "/apogeeutil/FieldObject.js";
+export {default as EventManager} from "/apogeejs-base-lib/src/EventManager.js";
+export {default as FieldObject} from "/apogeejs-base-lib/src/FieldObject.js";
